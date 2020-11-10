2d7db89fea088a1e926a2b4969bf7cd5cd7ed85a
Mike <m_kanyar@encs.concordia.ca>
2020-11-10 16:30:01 -0500

    run vs start from a thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,7 @@ package com.company;
 
 import org.w3c.dom.ls.LSOutput;
 
-import static com.company.ThreadColor.ANSI_GREEN;
-import static com.company.ThreadColor.ANSI_PURPLE;
+import static com.company.ThreadColor.*;
 
 public class Main {
 
@@ -12,13 +11,26 @@ public class Main {
 
         //create thread in the main function
         Thread anotherThread=new AnotherThread();
-        anotherThread.start();
+        anotherThread.setName("--Another Thread--");
+        //if we use the thread we created and then use .run it will run under the main thread. In other words it will run under the current main thread
+        // Thus if we want it to run from a new thread we have to start it
+        anotherThread.run();
+//        anotherThread.start();
         //anonymous class
         new Thread(){
             public void run(){
                 System.out.println(ANSI_GREEN+"Hello from the anonymous thread");
             }
         }.start();
+
+        //pass the runnable implementation as a parameter
+        Thread myRunnableThread=new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED+"hello from MyRunnable's implementation of run()");
+            }
+        });
+        myRunnableThread.start();
         System.out.println(ANSI_PURPLE+"Hello from the main thread");
         //cant start it for the second time it will create an error
 //        anotherThread.start();
